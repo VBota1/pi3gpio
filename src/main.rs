7462854d4beecb4b82b661a482e18f8d6d1443b0
V Bota <VBota1@users.noreply.github.com>
2017-05-20 16:51:39 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// Copyright 2017 Viorel Bota
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 extern crate rppal;
 
 use std::env;
@@ -203,3 +208,55 @@ fn set_pins (mut cmd: env::ArgsOs) {
 		argument.set_state( pin_state, &mut gpio );
 	}	
 }
+
+// For library "rppal" version "0.1.2" the following LICENSE (MIT) applyes:
+// "
+//   Copyright (c) 2017 Rene van der Meer
+// 
+//   Permission is hereby granted, free of charge, to any person obtaining a
+//   copy of this software and associated documentation files (the "Software"),
+//   to deal in the Software without restriction, including without limitation
+//   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//   and/or sell copies of the Software, and to permit persons to whom the
+//   Software is furnished to do so, subject to the following conditions:
+// 
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+// 
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//   DEALINGS IN THE SOFTWARE.
+// "
+
+// For the "std" rust library the following LINCESE (MIT) applyes:
+// "
+//   Copyright (c) 2010 The Rust Project Developers
+// 
+//   Permission is hereby granted, free of charge, to any
+//   person obtaining a copy of this software and associated
+//   documentation files (the "Software"), to deal in the
+//   Software without restriction, including without
+//   limitation the rights to use, copy, modify, merge,
+//   publish, distribute, sublicense, and/or sell copies of
+//   the Software, and to permit persons to whom the Software
+//   is furnished to do so, subject to the following
+//   conditions:
+// 
+//   The above copyright notice and this permission notice
+//   shall be included in all copies or substantial portions
+//   of the Software.
+// 
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//   ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//   TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//   PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+//   SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//   IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//   DEALINGS IN THE SOFTWARE.
+// "
